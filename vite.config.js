320d5f7617035b4de17152b9095fc3a962e5ef12
Herrie-Creator <kloppers.herman@gmail.com>
2026-05-28 10:40:00 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
